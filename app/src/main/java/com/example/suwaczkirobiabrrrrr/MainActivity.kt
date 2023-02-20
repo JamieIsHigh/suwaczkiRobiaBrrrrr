8be3f1fac1c631ac9b3fc5af29ae552438cf8f10
JamieIsHigh <113109707+JamieIsHigh@users.noreply.github.com>
2023-02-20 19:23:10 +0100

    working reset button

--- app/src/main/java/com/example/suwaczkirobiabrrrrr/MainActivity.kt
+++ app/src/main/java/com/example/suwaczkirobiabrrrrr/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.suwaczkirobiabrrrrr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bt = findViewById<Button>(R.id.button)
+        val im1 = findViewById<ImageView>(R.id.imageView)
+        val im2 = findViewById<ImageView>(R.id.imageView2)
+        val im3 = findViewById<ImageView>(R.id.imageView3)
+
+        bt.setOnClickListener {
+            im1.scaleX = 1.toFloat()
+            im1.scaleY = 1.toFloat()
+            im2.scaleX = 1.toFloat()
+            im2.scaleY = 1.toFloat()
+            im3.scaleX = 1.toFloat()
+            im3.scaleY = 1.toFloat()
+        }
     }
 }
